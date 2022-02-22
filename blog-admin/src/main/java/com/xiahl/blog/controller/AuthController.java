dc8ec9cb92c054266da365d7cff66f8f004df568
xiahl <1169555812@qq.com>
2022-02-22 19:34:03 +0800

    Revert "20220222 增加权限访问接口，数据库改为服务器上的数据库"
    This reverts commit 679c8685

--- blog-admin/src/main/java/com/xiahl/blog/controller/AuthController.java
+++ blog-admin/src/main/java/com/xiahl/blog/controller/AuthController.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -26,7 +25,6 @@ import java.util.Objects;
  * @date 1月29, 2022
  */
 @RestController
-@RequestMapping("auth")
 public class AuthController {
 
     @Autowired
