c2664b8862ecdee606b81051282cb5453ca9a225
“xiahaolei” <1169555812@qq.com>
2022-03-09 06:48:21 +0800

    add:提交 日志处理

--- blog-admin/src/main/java/com/xiahl/blog/aspect/LogAspect.java
+++ blog-admin/src/main/java/com/xiahl/blog/aspect/LogAspect.java
@@ -1,11 +1,20 @@
 package com.xiahl.blog.aspect;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author 夏浩磊
@@ -23,19 +32,35 @@ public class LogAspect {
     }
 
     @Before("log()")
-    public void doBefore() {
-        log.info("------------------------doBefore----------------");
+    public void doBefore(JoinPoint point) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+        String url = request.getRequestURL().toString();
+        String ip = request.getRemoteAddr();
+        String classMethod = point.getSignature().getDeclaringTypeName() + "." + point.getSignature().getName();
+        Object[] args = point.getArgs();
+        RequestLog requestLog = new RequestLog(url, ip, classMethod, args);
+        log.info("Request : {}", requestLog);
     }
 
 
     @Before("log()")
     public void doAfter() {
-        log.info("------------------------doAfter---------------");
     }
 
 
     @AfterReturning(returning = "result", pointcut = "log()")
-    public void doAfterReturn(Object result){
-        log.info("Result {}" ,result);
+    public void doAfterReturn(Object result) {
+        log.info("Result {}", result);
+    }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Data
+    private class RequestLog {
+        private String url;
+        private String ip;
+        private String classMethod;
+        private Object[] args;
     }
 }
