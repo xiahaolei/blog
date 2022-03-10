71202a40f935ee8415827084d3361c700f9c9e98
xiahl <1169555812@qq.com>
2022-03-10 10:23:51 +0800

    提交 fragments.html修改

--- blog-admin/src/main/java/com/xiahl/blog/controller/IndexController.java
+++ blog-admin/src/main/java/com/xiahl/blog/controller/IndexController.java
@@ -4,6 +4,7 @@ import com.xiahl.blog.Exception.MyNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author 夏浩磊
